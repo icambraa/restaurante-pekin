8342c6b38eb6b123ed20bf76b8961b2fcf4fbf8f
icambraa <126367844+icambraa@users.noreply.github.com>
2025-06-03 22:28:45 +0200

    rollback

--- src/js/preload-lcp.js
+++
@@ -1,7 +0,0 @@
-import restauranteFondo from "../img/restaurante-chino1.webp";
-
-const preload = document.createElement("link");
-preload.rel = "preload";
-preload.as = "image";
-preload.href = restauranteFondo;
-document.head.appendChild(preload);
